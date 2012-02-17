0bf208a3519e81401313077c76ef3e6dfe44ee02
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2012-02-17 00:01:41 +0000

    Added Smoke grenades by Tommo Updates related to NeuroPlanes.

---
+++ lua/entities/evac_indicator_green/shared.lua
@@ -0,0 +1,31 @@
+ENT.Type = "anim"
+
+ENT.Author			= "Tommo :D"
+ENT.Contact			= ""
+ENT.PrintName			= "Signalling Smoke-Grenade (Green)"
+ENT.Purpose			= "Requesting Backup"
+ENT.Instructions			= "Left click to deploy a green smoke grenade."
+
+/*---------------------------------------------------------
+OnRemove
+---------------------------------------------------------*/
+function ENT:OnRemove()
+end
+
+/*---------------------------------------------------------
+PhysicsUpdate
+---------------------------------------------------------*/
+function ENT:PhysicsUpdate()
+end
+
+/*---------------------------------------------------------
+PhysicsCollide
+---------------------------------------------------------*/
+function ENT:PhysicsCollide(data,phys)
+	if data.Speed > 50 then
+		self.Entity:EmitSound(Sound("HEGrenade.Bounce"))
+	end
+	
+	local impulse = -data.Speed * data.HitNormal * .4 + (data.OurOldVelocity * -.6)
+	phys:ApplyForceCenter(impulse)
+end
