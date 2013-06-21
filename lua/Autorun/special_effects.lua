c09e43120eb18c71e74f3b1ec3d12e789772b91c
Hoffa <Hoffa@14411003-878c-4423-8c1e-7edd909e0a33>
2013-06-21 21:05:08 +0000

    Added special_effects.lua

---
+++ lua/Autorun/special_effects.lua
@@ -0,0 +1,5 @@
+-- This is where we register custom particle effect systems.
+game.AddParticles("particles/vman_explosion.pcf")
+game.AddParticles("particles/neuro_tank_he.pcf")
+game.AddParticles("particles/neuro_tank_ap.pcf")
+game.AddParticles("particles/neuro_ricochet.pcf")
