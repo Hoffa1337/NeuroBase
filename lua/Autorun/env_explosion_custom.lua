ed0ea46e19f73223d74a5ac915991e0a61a98c75
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2013-06-21 12:24:31 +0000

    New Particle Effects.

--- lua/Autorun/env_explosion_custom.lua
+++ lua/Autorun/env_explosion_custom.lua
@@ -8,18 +8,19 @@ function Splode()
 		if( v:IsValid() ) then
 			
 			
+			
 			local Pos = v:LocalToWorld( v:OBBCenter( ) )
 			if( v.DoCustomExplosion ) then
 				
-				ParticleEffect(v.CustomParticleEffect, Pos, Angle(0,0,0), nil )
+				-- ParticleEffect( v.CustomParticleEffect, Pos, Angle(0,0,0), nil )
 			
 			else
 			
-				ParticleEffect("dusty_explosion_rockets", Pos, Angle(0,0,0), nil )
+				-- ParticleEffect("dusty_explosion_rockets", Pos, Angle(0,0,0), nil )
 				
 			end
 			
-			v:Remove()
+			-- v:Remove()
 		
 		end
 		
