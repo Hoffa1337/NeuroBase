05e5a19ad11ee1d0369ad57c2a7bf2eb24d2ce4b
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2013-06-23 08:58:51 +0000

    Added Vmans new special fx.

--- lua/Autorun/special_effects.lua
+++ lua/Autorun/special_effects.lua
@@ -1,5 +1,10 @@
 -- This is where we register custom particle effect systems.
 game.AddParticles("particles/vman_explosion.pcf")
 game.AddParticles("particles/neuro_tank_he.pcf")
-game.AddParticles("particles/neuro_tank_ap.pcf")
-game.AddParticles("particles/neuro_ricochet.pcf")
+game.AddParticles("particles/neuro_tank_ap.pcf") 
+game.AddParticles("particles/neuro_ricochet.pcf") -- tank_ric
+game.AddParticles("particles/neuro_tankfire.pcf") -- tank_fire
+game.AddParticles("particles/neuro_tank_flash.pcf") -- tank_muzzleflash
+game.AddParticles("particles/neuro_cookoff.pcf") -- tank_cookoff 
+
+print( "NeuroTec Special Effects loaded" )
