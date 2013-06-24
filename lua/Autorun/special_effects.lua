5b921177689e95524a04c7dcb8f04c6358f879d0
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2013-06-24 10:38:31 +0000

    Registered new effects.

--- lua/Autorun/special_effects.lua
+++ lua/Autorun/special_effects.lua
@@ -1,5 +1,8 @@
 -- This is where we register custom particle effect systems.
 game.AddParticles("particles/vman_explosion.pcf")
+game.AddParticles("particles/neuro_tank_flash.pcf") --AA_muzzleflash
+-- game.AddParticles("particles/apc_muzzleflash.pcf") --apc_muzzleflash
+game.AddParticles("particles/neuro_gore.pcf") -- tank_gore
 game.AddParticles("particles/neuro_tank_he.pcf")
 game.AddParticles("particles/neuro_tank_ap.pcf") 
 game.AddParticles("particles/neuro_ricochet.pcf") -- tank_ric
