945c0ecce0fae8ba3eba343cd9c87f3a507d26a4
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2013-07-05 23:43:23 +0000

    particle manifest

--- lua/Autorun/special_effects.lua
+++ lua/Autorun/special_effects.lua
@@ -12,5 +12,6 @@ game.AddParticles("particles/neuro_cookoff.pcf") -- tank_cookoff
 game.AddParticles("particles/neuro_trails.pcf") --  rockettrail
 game.AddParticles("particles/neuro_v1.pcf") --  v1_impact
 game.AddParticles("particles/neuro_ied.pcf") --  VBIED_explosion
+game.AddParticles("particles/neuro_rocket.pcf") --  rocket_impact_dirt  rocket_impact_wall
 -- rocketrail/s
 print( "NeuroTec Special Effects loaded" )
