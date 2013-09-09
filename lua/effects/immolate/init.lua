473164c5df5686ad07d8ec3badc52bc8ca3885c1
Hoffa <Hoffa@14411003-878c-4423-8c1e-7edd909e0a33>
2013-09-09 08:31:42 +0000

    Boatstuff

--- lua/effects/immolate/init.lua
+++ lua/effects/immolate/init.lua
@@ -17,17 +17,17 @@ function EFFECT:Init( data )
 	local emitter = ParticleEmitter( Pos )
 	
 	--firecloud
-		for i=1, 4 do
+		for i=1, 6 do
 		
-			local particle = emitter:Add( "particles/flamelet"..math.random( 1, 5 ), Pos + Vector(math.random(-2,2),math.random(-2,2),math.random(-2,2)))
+			local particle = emitter:Add( "particles/flamelet"..math.random( 1, 5 ), Pos + Vector(math.random(-12,12),math.random(-12,12),math.random(-2,2)))
 				
 				if( particle ) then
 				
-					particle:SetVelocity( Vector(math.random(-30,30),math.random(-30,30),math.random(45,190)) )
-					particle:SetDieTime( math.Rand( 0.2, 0.9 ) )
-					particle:SetStartAlpha( math.random( 200, 240 ) )
-					particle:SetStartSize( 0 )
-					particle:SetEndSize( math.random( 28, 94 ) )
+					particle:SetVelocity( Vector(math.random(-30,30),math.random(-30,30),math.random( -50,290 ) ) )
+					particle:SetDieTime( math.Rand( 0.5, 0.9 ) )
+					particle:SetStartAlpha( math.random( 233, 255 ) )
+					particle:SetStartSize( math.random(  15, 31 ) )
+					particle:SetEndSize( math.random( 1, 2 ) )
 					particle:SetRoll( math.random( 360, 480 ) )
 					particle:SetRollDelta( math.Rand( -1, 1 ) )
 					particle:SetColor( math.random( 150, 255 ), math.random( 100, 150 ), 100 )
@@ -44,13 +44,13 @@ function EFFECT:Init( data )
 			
 			if( particle ) then
 					
-				particle:SetVelocity( Vector(math.random(-60,60),math.random(-66,70),math.random(55,200)) )
+				particle:SetVelocity( Vector(math.random(-25,25),math.random(-25,25),math.random(55,200)) )
 				particle:SetDieTime( math.Rand( 2, 7 ) )
-				particle:SetStartAlpha( math.random( 35, 55 ) )
+				particle:SetStartAlpha( math.random( 135, 155 ) )
 				particle:SetEndAlpha( math.random( 0, 1 ) )
 				particle:SetStartSize( math.random( 5,20 ) )
 				particle:SetEndSize( math.random( 60, 150 ) )
-				particle:SetRoll( math.random( 360, 480 ) )
+				particle:SetRoll( math.random( 1, 480 ) )
 				particle:SetRollDelta( math.Rand( -1, 1 ) )
 				particle:SetColor( 20, 20, 20 )
 				particle:VelocityDecay( false )
