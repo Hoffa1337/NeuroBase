69e7c39c0a0335b6916a87c43ea9e93c736ba1e2
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2014-03-24 22:06:09 +0000

    Added FAB vfx by The Doctor.

--- lua/Autorun/special_effects.lua
+++ lua/Autorun/special_effects.lua
@@ -1,9 +1,9 @@
 -- This is where we register custom particle effect systems.
-game.AddParticles("particles/vman_explosion.pcf")
+game.AddParticles("particles/vman_explosion.pcf") -- dusty_rockets
 -- game.AddParticles("particles/neuro_tank_flash.pcf") --AA_muzzleflash
 game.AddParticles("particles/neuro_tank_arty.pcf")
 game.AddParticles("particles/neuro_gore.pcf") -- tank_gore
-game.AddParticles("particles/neuro_tank_he.pcf") --
+game.AddParticles("particles/neuro_tank_he.pcf") -- 
 game.AddParticles("particles/neuro_tank_ap.pcf") -- 30cal_impact
 game.AddParticles("particles/neuro_ricochet.pcf") -- tank_ric
 game.AddParticles("particles/neuro_tankfire.pcf") -- tank_fire
@@ -20,6 +20,7 @@ game.AddParticles("particles/neuro_bombs.pcf") --  FAB_Explosion FAB_groundwave 
 game.AddParticles("particles/neuro_flamethrower.pcf") --  FAB_Explosion FAB_groundwave carpet_explode
 game.AddParticles("particles/neuro_gascan.pcf") -- neuro_gascan_explo - neuro_gascan_explo_air - air
 game.AddParticles("particles/neuro_nuke_lowyield.pcf") --  lowyield_nuke_air_main lowyield_nuke_ground_main 
+game.AddParticles("particles/fuel_air_bomb.pcf") --  neuro_fab_main_ground neuro_fab_main_air 
 
 -- rocketrail/s
 print( "NeuroTec Special Effects loaded" )
