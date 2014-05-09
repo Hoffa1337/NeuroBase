ff7f5975862a9d745ebfde4ec68566a430ab1b4d
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2014-05-09 16:27:14 +0000

    Tweaks by Rebel324

--- lua/entities/sent_a2a_jericho_drone/cl_init.lua
+++ lua/entities/sent_a2a_jericho_drone/cl_init.lua
@@ -2,56 +2,80 @@
 include('shared.lua')
 local matHeatWave		= Material( "sprites/heatwave" )
 local matFire			= Material( "effects/fire_cloud1" )
+local emitter = ParticleEmitter(Vector(0, 0, 0))
 function ENT:Initialize()
 	self.Seed = math.Rand( 0, 10000 )
+	self.cooltime = CurTime()
+	self.lit = false
+	timer.Simple(.4, function() self.lit = true end)
 end
 
 function ENT:Draw()
 	self.Entity:DrawModel()
-	self.OnStart = self.OnStart or CurTime()
-	self:EffectDraw_fire()
-	
 end
 
 function ENT:OnRemove()
 end
 
 
+function ENT:Think()
+	if (self.cooltime < CurTime()) then
+		if self.lit then
+			local smoke = emitter:Add("effects/smoke_a", self:GetPos() + self:GetForward()*-50)
+			smoke:SetVelocity(self:GetForward()*-800)
+			smoke:SetDieTime(math.Rand(.9,1.2))
+			smoke:SetStartAlpha(math.Rand(200,240))
+			smoke:SetEndAlpha(0)
+			smoke:SetStartSize(math.random(14,18))
+			smoke:SetEndSize(math.random(66,99))
+			smoke:SetRoll(math.Rand(180,480))
+			smoke:SetRollDelta(math.Rand(-2,2))
+			smoke:SetGravity( Vector( 0, math.random(1,90), math.random(51,155) ) )
+			smoke:SetColor(50, 50, 50)
+			smoke:SetAirResistance(60)
 
+			local fire = emitter:Add("effects/smoke_a", self:GetPos() + self:GetForward()*-50)
+			fire:SetVelocity(self:GetForward()*-10)
+			fire:SetDieTime(math.Rand(.05,.1))
+			fire:SetStartAlpha(math.Rand(222,255))
+			fire:SetEndAlpha(0)
+			fire:SetStartSize(math.random(11,14))
+			fire:SetEndSize(math.random(20,33))
+			fire:SetAirResistance(150)
+			fire:SetRoll(math.Rand(180,480))
+			fire:SetRollDelta(math.Rand(-3,3))
+			fire:SetStartLength(15)
+			fire:SetColor(255,100,0)
+			fire:SetEndLength(math.Rand(100, 150))
+
+			local fire = emitter:Add("effects/smoke_a", self:GetPos() + self:GetForward()*-50)
+			fire:SetVelocity(self:GetForward()*-10)
+			fire:SetDieTime(math.Rand(.05,.1))
+			fire:SetStartAlpha(math.Rand(222,255))
+			fire:SetEndAlpha(0)
+			fire:SetStartSize(math.random(11,14))
+			fire:SetEndSize(math.random(20,33))
+			fire:SetAirResistance(150)
+			fire:SetRoll(math.Rand(180,480))
+			fire:SetRollDelta(math.Rand(-3,3))
+			fire:SetColor(255,110,0)
+
+			local fire = emitter:Add("effects/yellowflare", self:GetPos() + self:GetForward()*-50)
+			fire:SetVelocity(self:GetForward()*-10)
+			fire:SetDieTime(math.Rand(.03,.05))
+			fire:SetStartAlpha(math.Rand(222,255))
+			fire:SetEndAlpha(0)
+			fire:SetStartSize(math.random(77,88))
+			fire:SetEndSize(math.random(99,100))
+			fire:SetAirResistance(150)
+			fire:SetRoll(math.Rand(180,480))
+			fire:SetRollDelta(math.Rand(-3,3))
+			fire:SetColor(255,120,0)
+		end
+		self.cooltime = CurTime() + .03
+	end
+end
+
+// Thanks garry :D
 function ENT:EffectDraw_fire()
-
-	local vOffset = self:GetPos() + self:GetForward() * -5
-	local vNormal = (vOffset - self:GetPos()):GetNormalized()
-
-	local scroll = self.Seed + (CurTime() * -10)
-	
-	local Scale = math.Clamp( (CurTime() - self.OnStart) * 5, 0, 1 )
-		
-	render.SetMaterial( matFire )
-	
-	render.StartBeam( 3 )
-		render.AddBeam( vOffset, 4 * Scale, scroll, Color( 0, 0, 255, 128) )
-		render.AddBeam( vOffset + vNormal * 30 * Scale, 16 * Scale, scroll + 1, Color( 255, 255, 255, 128) )
-		render.AddBeam( vOffset + vNormal * 50 * Scale, 16 * Scale, scroll + 3, Color( 255, 255, 255, 0) )
-	render.EndBeam()
-	
-	scroll = scroll * 0.5
-	
-	render.UpdateRefractTexture()
-	render.SetMaterial( matHeatWave )
-	render.StartBeam( 3 )
-		render.AddBeam( vOffset, 8 * Scale, scroll, Color( 0, 0, 255, 128) )
-		render.AddBeam( vOffset + vNormal * 16 * Scale, 16 * Scale, scroll + 2, Color( 255, 255, 255, 255) )
-		render.AddBeam( vOffset + vNormal * 64 * Scale, 24 * Scale, scroll + 5, Color( 0, 0, 0, 0) )
-	render.EndBeam()
-	
-	
-	scroll = scroll * 1.3
-	render.SetMaterial( matFire )
-	render.StartBeam( 3 )
-		render.AddBeam( vOffset, 4 * Scale, scroll, Color( 0, 0, 255, 128) )
-		render.AddBeam( vOffset + vNormal * 30 * Scale, 8 * Scale, scroll + 1, Color( 255, 255, 255, 128) )
-		render.AddBeam( vOffset + vNormal * 62 * Scale, 8 * Scale, scroll + 3, Color( 255, 255, 255, 0) )
-	render.EndBeam()
-	
 end
