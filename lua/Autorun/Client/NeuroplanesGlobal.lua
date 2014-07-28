5e77715e7e4ebb2da7d7919c7e24414db501a0c0
Hoffa <Hoffa@14411003-878c-4423-8c1e-7edd909e0a33>
2014-07-28 10:43:05 +0000

    Added JetAir Framework for combat jets. See Gloster Meteor for reference.

--- lua/Autorun/Client/NeuroplanesGlobal.lua
+++ lua/Autorun/Client/NeuroplanesGlobal.lua
@@ -4,6 +4,130 @@ function Meta:SetShouldDrawInViewMode( a )
 return
 end
 
+local matHeatWave		= Material( "sprites/heatwave" )
+local matFire			= Material( "effects/fire_cloud1" )
+local matPlasma			= Material( "effects/strider_muzzle" )
+local matSprite 		= Material( "sprites/gmdm_pickups/light"  )
+
+
+function Meta:JetAir_DrawJetFlames( pos )
+
+	local vOffset = pos + self:GetForward() * -1
+	local vNormal = ( vOffset - pos ):GetNormalized()
+	local Throttle = math.Clamp( self:GetVelocity():Length() / (1.8 * 965) + 0.01,0, 10 )
+	local scroll = CurTime() * -20
+	local Scale = 1.5 * Throttle
+	
+	render.SetMaterial( matHeatWave )
+	
+	scroll = scroll * 0.9
+	render.StartBeam( 3 )
+		render.AddBeam( vOffset, 16, scroll, Color( 0, 255, 255, 205 ) )
+		render.AddBeam( vOffset + vNormal * 16 * Scale, 32, scroll + 0.01, Color( 255, 255, 255, 205 ) )
+		render.AddBeam( vOffset + vNormal * 128 * Scale, 32, scroll + 0.02, Color( 0, 255, 255, 15 ) )
+	render.EndBeam()
+
+	scroll = scroll * 0.9
+	
+	render.StartBeam( 3 )
+		render.AddBeam( vOffset, 32, scroll, Color( 0, 255, 255, 255) )
+		render.AddBeam( vOffset + vNormal * 16 * Scale, 32, scroll + 0.01, Color( 255, 255, 255, 255 ) )
+		render.AddBeam( vOffset + vNormal * 128 * Scale, 32, scroll + 0.02, Color( 0, 255, 255, 0 ) )
+	render.EndBeam()
+	
+	scroll = scroll * 0.6
+	
+	render.StartBeam( 3 )
+		render.AddBeam( vOffset, 32, scroll, Color( 0, 255, 255, 255) )
+		render.AddBeam( vOffset + vNormal * 16 * Scale, 32, scroll + 0.01, Color( 255, 255, 255, 255 ) )
+		render.AddBeam( vOffset + vNormal * 128 * Scale, 32, scroll + 0.02, Color( 0, 255, 255, 0 ) )
+	render.EndBeam()
+
+	scroll = scroll * 0.5
+	render.UpdateRefractTexture()
+	
+	render.StartBeam( 3 )
+		render.AddBeam( vOffset, 16 * Scale, scroll, Color( 0, 0, 255, 128) )
+		render.AddBeam( vOffset + vNormal * 32 * Scale, 32 * Scale, scroll + 2, Color( 255, 255, 255, 255 ) )
+		render.AddBeam( vOffset + vNormal * 128 * Scale, 48 * Scale, scroll + 5, Color( 0, 0, 0, 0 ) )
+	render.EndBeam()
+	
+	local extra = math.Clamp( self:GetVelocity():Length() / (1.8 * 965),0, 25 )
+	render.SetMaterial( matSprite )
+	render.DrawSprite( pos, 32 + extra, 32 + extra, Color( 25,25,255,165 ) )
+	
+end
+
+function Meta:JetAir_Draw()
+	local p = LocalPlayer()
+	self:DrawModel()
+	
+	if( self.ExhaustPositions ) then
+		
+		for i = 1, #self.ExhaustPositions do
+			
+			self:JetAir_DrawJetFlames( self.ExhaustPositions[i] )
+
+		end
+		
+	end
+	
+end
+
+function Meta:JetAir_CView( ply, Origin, Angles, Fov )
+
+	local plane = ply:GetScriptedVehicle()
+
+	local view = {
+		origin = Origin,
+		angles = Angles
+		}
+
+	if( IsValid( plane ) && ply:GetNetworkedBool( "InFlight", false )  ) then
+
+		local pos
+		
+		if( GetConVarNumber("jet_cockpitview") > 0 ) then
+				
+			pos = plane:LocalToWorld( self.CockpitCameraPos ) //Origin//
+			
+		else
+			
+			pos = plane:GetPos() + plane:GetUp() * plane.CamUp + ply:GetAimVector() * -plane.CamDist
+		
+		end
+		
+		local isGuidingRocket = plane:GetNetworkedBool( "DrawTracker", false )
+		local fov = GetConVarNumber("fov_desired")
+		local ang,pAng = ply:EyeAngles(), plane:GetAngles()
+
+		if ( isGuidingRocket ) then
+
+			pos = plane:GetPos() + plane:GetUp() * -150
+			fov = 60
+			
+		end
+		
+		if ( ply:KeyDown( IN_ATTACK ) ) then
+				
+				ang.p = ang.p + math.Rand(-.2,.2)
+				ang.y = ang.y + math.Rand(-.2,.2)
+					
+		end
+		
+		view = {
+			origin = pos,
+			angles = ang,-- / 2.2 ),
+			fov = fov
+			}
+
+	end
+
+	return view
+
+	
+end
+
 -- Global client stuff
 local ScrW = ScrW
 local ScrH = ScrH
