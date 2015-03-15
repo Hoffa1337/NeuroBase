acebf9a16bda6de4ddbeca91d12dd65bbf0e04a1
smithy285 <smithy285@14411003-878c-4423-8c1e-7edd909e0a33>
2015-03-15 14:18:39 +0000

    list.Set hasn't been used in Garry's Mod for over a year #FeelOldYetHoffa?

--- lua/Autorun/neurotec_playermodels.lua
+++ lua/Autorun/neurotec_playermodels.lua
@@ -1,8 +1,4 @@
 local function AddPlayerModel( name, model )
-
-    list.Set( "PlayerOptionsModel", name, model )
     player_manager.AddValidModel( name, model )
-    
 end
-
-AddPlayerModel("USAF Pilot", "models/player/pilot_usaf.mdl" )
+AddPlayerModel( "USAF Pilot", "models/player/pilot_usaf.mdl" )
