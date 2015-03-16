16a1524a9fae41f933033f8ad6923d35d599935f
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2015-03-16 15:15:20 +0000

    Testing something to prove someone on facepunch wrong.

--- lua/entities/sent_mini_napalm/cl_init.lua
+++ lua/entities/sent_mini_napalm/cl_init.lua
@@ -1,23 +1,6 @@
-
 include('shared.lua')
-
-function ENT:Initialize()
-end
-
 function ENT:Draw()
-	self.Entity:DrawModel()
-end
-
-
-function ENT:OnRemove()
-	
-	-- if( (LocalPlayer():GetPos() - self:GetPos() ):Length() < 10000 ) then
-	
-		-- surface.PlaySound( "ambient/explosions/exp"..math.random(1,4)..".wav" )
-		
-	-- end
-	
-
+	self:DrawModel()
 end
 
 
--- lua/entities/sent_mini_napalm/shared.lua
+++ lua/entities/sent_mini_napalm/shared.lua
@@ -1,9 +1,7 @@
-
 ENT.Base			= "base_anim"
 ENT.Type 			= "anim"
 ENT.PrintName		= ""
 ENT.Author			= "Hoffa"
-
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
 
