f3d20a5865e2a4532c8b5d5722f4caf340b4b8a7
Robin Hoffa <Hoffa1337@users.noreply.github.com>
2015-04-21 18:54:58 +0000

    tweaks so microplanes can use the torpedo.

--- lua/entities/sent_mini_torpedo/cl_init.lua
+++ lua/entities/sent_mini_torpedo/cl_init.lua
@@ -1,18 +1,18 @@
 
 include('shared.lua')
-local scale = Vector(0.2, 0.2, 0.2)
+-- local scale = Vector(0.2, 0.2, 0.2)
 function ENT:Initialize()
 	
-	local mat = Matrix()
-	mat:Scale(scale)
-	self:EnableMatrix("RenderMultiply", mat )
+	-- local mat = Matrix()
+	-- mat:Scale(scale)
+	-- self:EnableMatrix("RenderMultiply", mat )
 
 
 end
 
 function ENT:Draw()
 	
-	self:SetPos( self:GetPos() - Vector( 0,0,16 ) )
+	-- self:SetPos( self:GetPos() - Vector( 0,0,16 ) )
 	self:DrawModel()
 
 end
